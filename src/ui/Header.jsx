1d0a16e35ba9e38697cc4bb0575e14dc6c86b5d8
YTeyddie22 <teyddie@gmail.com>
2025-02-28 12:28:08 +0300

    global header

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <Link to="/">Pizz Co.</Link>
+            <p>Teddy</p>
+        </header>
+    );
+};
+
+export default Header;
