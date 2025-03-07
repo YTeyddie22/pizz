1b9818e5a5be1326b5ff529f34c79fa684df8c03
YTeyddie22 <teyddie@gmail.com>
2025-03-07 14:24:41 +0300

    add data using Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+    action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import AppLayout from "./ui/AppLayout";
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
             {
                 path: "/order/new",
                 element: <CreateOrder />,
+                action: createOrderAction,
             },
             {
                 path: "/order/:orderId",
