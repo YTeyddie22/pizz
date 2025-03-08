0b0fb5bd2d217b836e491b25d5ad63dd9e9de855
YTeyddie22 <teyddie@gmail.com>
2025-03-08 08:56:44 +0300

    fix navigation state

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import CartOverview from "../features/cart/CartOverview";
 import { Outlet, useNavigation } from "react-router-dom";
 
 const AppLayout = () => {
-    const loading = useNavigation.state === "loading";
+    const loading = useNavigation().state === "loading";
     return (
         <div className="layout">
             <Header />
