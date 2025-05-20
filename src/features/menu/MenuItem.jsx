112a3480e891710597b6c5259817230b81afe949
YTeyddie22 <teyddie@gmail.com>
2025-05-20 12:34:51 +0300

    get current quantity

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helper";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
     const dispatch = useDispatch();
     const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+    const currentQuantity = useSelector(getCurrentQuantityById(id));
+    const isInCart = currentQuantity > 0;
 
     function handleAddToCart() {
         const newItem = {
@@ -39,8 +42,8 @@ function MenuItem({ pizza }) {
                             Sold out
                         </p>
                     )}
-
-                    {!soldOut && (
+                    {isInCart && <DeleteItem pizzaId={id} />}
+                    {!soldOut && !isInCart && (
                         <Button type="small" onClick={handleAddToCart}>
                             Add to cart
                         </Button>
