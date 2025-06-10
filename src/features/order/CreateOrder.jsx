2b6776fab2b3e8cad61fe880dca4842c118354e8
YTeyddie22 <teyddie@gmail.com>
2025-06-10 10:54:48 +0300

    working test

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,42 +2,19 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helper";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
     /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
         str
     );
-/*
-const fakeCart = [
-    {
-        pizzaId: 12,
-        name: "Mediterranean",
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: "Vegetale",
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: "Spinach and Mushroom",
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
-];
-*/
+
 function CreateOrder() {
     const [withPriority, setWithPriority] = useState(false);
     const username = useSelector((state) => state.user.username);
@@ -45,6 +22,8 @@ function CreateOrder() {
     const isSubmitting = navigation.state === "submitting";
 
     const formErrors = useActionData();
+    const dispatch = useDispatch();
+
     const cart = useSelector(getCart);
     const totalCartPrice = useSelector(getTotalCartPrice);
     const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
@@ -57,7 +36,9 @@ function CreateOrder() {
             <h2 className="mb-8 text-xl font-semibold">
                 Ready to order? Let's go!
             </h2>
-
+            <button onClick={() => dispatch(fetchAddress())}>
+                Get Position
+            </button>
             {/* <Form method="POST" action="/order/new"> */}
             <Form method="POST">
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
